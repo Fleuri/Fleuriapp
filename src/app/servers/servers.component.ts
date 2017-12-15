64fe87bb208f5a2bdd1b74a3dc23abcfd1ae33a1
Fleuri <laur1.suomalainen@gmail.com>
2017-12-15 11:07:59 +0200

    Assignment 3

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html'
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
@@ -11,6 +11,10 @@ export class ServersComponent implements OnInit {
   serverName = 'New Server';
   userName = '';
   userNameInfo = 'Please enter your username';
+  servers = ['Testserver', 'Fleuriserver'];
+  detailButtonClickCount = 0;
+  showDetails = false;
+  timeStamps = [];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -22,6 +26,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     //this.serverCreationStatus = 'Server was created! Server name is ' + this.serverName;
   }
 
@@ -44,4 +49,21 @@ export class ServersComponent implements OnInit {
   stringEmpty() {
     return !(/\S/.test(this.userName));
   }
+
+  onDetailButtonClick() {
+    if (this.showDetails) {
+      this.showDetails = false;
+      } else {
+      this.showDetails = true;
+    }
+    var d = new Date();
+    var n = d.toString();
+    this.timeStamps.push(n);
+    }
+
+    getFifthColour(i) {
+      if (i >= 4) {
+        return 'blue';
+      }
+    }
 }
